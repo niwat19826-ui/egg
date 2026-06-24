9e62f27256361dfc7fb71751ee72986ed2a748b9
niwat19826-ui <niwat19826@example.com>
2026-06-24 10:14:24 +0700

    update

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,61 @@
-export default function Register() {
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+
+export default function RegisterPage() {
+  const router = useRouter();
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify(form),
+    });
+
+    if (res.ok) {
+      router.push("/login");
+    } else {
+      alert("Register failed");
+    }
+  }
+
+
+
+
   return (
     <div className="auth-page">
-      <form className="auth-card">        
-        <h1>เกี่ยวกับเรา</h1>
-        <input placeholder="j"/>       
+      <form className="auth-card" onSubmit={handleSubmit}>
+        <h1>Register</h1>
+
+        <input
+          placeholder="Name"
+          onChange={(e) => setForm({ ...form, name: e.target.value })}
+        />
+
+        <input
+          placeholder="Email"
+          type="email"
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+        />
+
+        <input
+          placeholder="Password"
+          type="password"
+          onChange={(e) => setForm({ ...form, password: e.target.value })}
+        />
+
+        <button>Register</button>
       </form>
     </div>
   );
