823ea32d672bf29f0ce3a1bbfef7275fbd88a964
niwat19826-ui <niwat19826@example.com>
2026-08-26 09:52:34 +0700

    update\

--- app/admin/blogs/page.tsx
+++ app/admin/blogs/page.tsx
@@ -1,10 +1,11 @@
 import BlogForm from "@/components/BlogForm";
+import BlogCard from "@/components/BlogCard";
 
 export default function CategoriesPage() {
     return (
         <div>
-            <></>
             <BlogForm/>
+            <BlogCard />
         </div>
     );
 }
